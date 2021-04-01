6482992c2d4b2863a706159823edbe34e8c69d26
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-03-31 22:58:07 -0300

    lógica editar e deletar estoque e franquias

--- src/main/java/com/franquia/controller/EstoqueController.java
+++ src/main/java/com/franquia/controller/EstoqueController.java
@@ -3,10 +3,10 @@ package com.franquia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.franquia.model.Estoque;
 import com.franquia.service.EstoqueService;
 
@@ -15,6 +15,7 @@ public class EstoqueController {
 	
 	@Autowired
 	private EstoqueService estoqueService;
+
 
 	@GetMapping("/home")
 	public String index() {
@@ -31,9 +32,9 @@ public class EstoqueController {
 	public ModelAndView postCriarEstoque(Estoque estoque) {
 		estoqueService.criarEstoque(estoque);
 		ModelAndView model = new ModelAndView("franqueador/criarEstoque");
-		Iterable<Estoque> estoques1 = estoqueService.listarEstoque();
-		model.addObject("estoques", estoques1);
-		model.addObject("mensagem", "Estoque salvo com sucesso");
+		Iterable<Estoque> estoques = estoqueService.listarEstoque();
+		model.addObject("estoques", estoques);
+		model.addObject("mensagemCadastrada", "Estoque salvo com sucesso");
  		return model;
 	}
 	
@@ -44,6 +45,24 @@ public class EstoqueController {
 		model.addObject("estoques", estoques);
 		return model;
 	}
+	
+	@RequestMapping("/deletar-estoque")
+	public ModelAndView deleteEstoque(Long id) {
+		Estoque estoque = estoqueService.idEstoque(id);
+		estoqueService.deletarEstoque(estoque);
+		ModelAndView model = new ModelAndView("redirect:/ver-estoque");
+		Iterable<Estoque> estoques1 = estoqueService.listarEstoque();
+		model.addObject("estoque", estoques1);
+		return model;
+	}
 
 	
+	@GetMapping("/editar-estoque")
+    public ModelAndView updateEditarLivro(@RequestParam Long id){
+    ModelAndView model = new ModelAndView("franqueador/editarEstoque");
+    Estoque estoque = estoqueService.idEstoque(id);
+    model.addObject("estoque",  estoque);
+    model.addObject("mensagemEditada", "Estoque será editado");
+    return model;
+	}
 }
