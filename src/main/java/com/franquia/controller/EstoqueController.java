6ff67af115105486541036b25d20a9f6ec1d4471
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-05-18 00:14:35 -0300

    criar método buscar na page estoque e page resultado estoque

--- src/main/java/com/franquia/controller/EstoqueController.java
+++ src/main/java/com/franquia/controller/EstoqueController.java
@@ -1,17 +1,16 @@
 package com.franquia.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.franquia.model.Estoque;
 import com.franquia.service.EstoqueService;
-import com.franquia.service.MarcaService;
-import com.franquia.service.ProdutoService;
-import com.franquia.service.VendaService;
 
 @Controller
 public class EstoqueController {
@@ -59,15 +58,36 @@ public class EstoqueController {
 		return model;
 		}
 
-	
-	
-	
+
 	@GetMapping("/editar-estoque")
-    public ModelAndView updateEditarLivro(@RequestParam Long id){
+    public ModelAndView updateEditarEstoque(@RequestParam Long id){
     ModelAndView model = new ModelAndView("franqueador/estoque/editarEstoque");
     Estoque estoque = estoqueService.idEstoque(id);
     model.addObject("estoque",  estoque);
     model.addObject("mensagemEditada", "Estoque será editado");
     return model;
 	}
+	
+	
+	@PostMapping("/pesquisar-estoque")
+	public ModelAndView findEstoque(@RequestParam(required = false) String nome_estoque) {
+		ModelAndView model = new ModelAndView();
+		List<Estoque> pesquisarEstoque;
+		
+		if(nome_estoque == null || nome_estoque.trim().isEmpty()) {
+			pesquisarEstoque = (List<Estoque>) estoqueService.listarEstoque();
+		} else {
+			pesquisarEstoque = estoqueService.pesquisarEstoque(nome_estoque);
+		}
+		
+		model.addObject("pesquisarEstoque", pesquisarEstoque);
+		model.setViewName("franqueador/estoque/pesquisarEstoque");
+		return model;
+	} 
+	
+	//@GetMapping("/pesquisar-estoque")
+	//public String pesquisarNomeEstoque(@RequestParam("nome_estoque") String estoque_nome, ModelMap model) {
+		//model.addAttribute("estoque", estoqueService.buscarPorNome(estoque_nome));
+		//return "franqueador/estoque/pesquisarEstoque";
+	//}
 }
--- src/main/java/com/franquia/repository/EstoqueRepository.java
+++ src/main/java/com/franquia/repository/EstoqueRepository.java
@@ -1,10 +1,14 @@
 package com.franquia.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.franquia.model.Estoque;
 
 @Repository
-public interface EstoqueRepository extends CrudRepository<Estoque, Long> {
+public interface EstoqueRepository extends CrudRepository<Estoque, Long>  {
+
+	
 
 }
