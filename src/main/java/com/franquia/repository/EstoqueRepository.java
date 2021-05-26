dfec54412c2c7da6270818ce9dfe47ce0988b778
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-05-25 22:28:06 -0300

    Removendo acesso de dados da interface Estoque Repository

--- src/main/java/com/franquia/repository/EstoqueRepository.java
+++ src/main/java/com/franquia/repository/EstoqueRepository.java
@@ -1,7 +1,5 @@
 package com.franquia.repository;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.franquia.model.Estoque;
